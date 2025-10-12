87788274946862324594a1627ae964b9c91b6b18
bteena04 <teenabusgeeth04@hotmail.com>
2025-10-12 23:46:07 +0400

    Added pages navigation logic

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,6 +1,7 @@
 // js/components/navbar.js
 Vue.component('navbar', {
   template: '#navbar_template',
+  props: ['currentView'], // Receive currentView as a prop from parent.
   data: function() {
     return {
       darkMode: true,
@@ -10,12 +11,19 @@ Vue.component('navbar', {
       },
       userLoginStatus: { 
         isUserLoggedIn: true 
-      }
+      }//,
+    //   navigateHtml: 'nav-link active aria-current="page" href="#" @click.prevent="go(\'' + view + '\')"' 
     };
   },
   methods: {
     toggleTheme: function() {
       this.darkMode = !this.darkMode;
+    },
+    go: function(view) {
+      this.$emit('navigate', view);
+    },
+    isActive: function(view) {
+      return this.currentView === view ? 'active' : '';
     }
   }
 });
--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    
+    currentView: 'home' // Default view
+  },
+  methods: {
+    navigate: function(view) {
+      this.currentView = view;
+    }
   }
 });
 
