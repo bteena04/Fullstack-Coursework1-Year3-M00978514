0bdb8b76d89989f9df34412d967e9e61fcf24138
bteena04 <teenabusgeeth04@hotmail.com>
2025-11-23 15:03:39 +0400

    Remove trailing slash in backend URL; Modified CSS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    backendUrl: 'https://fullstack-coursework1-year3-expressapp.onrender.com/', //https://fullstack-coursework1-year3-expressapp.onrender.com/
+    backendUrl: 'https://fullstack-coursework1-year3-expressapp.onrender.com', //https://fullstack-coursework1-year3-expressapp.onrender.com/
     serverStatus: null, // null = unknown, true = up, false = down
     currentView: 'home',
     darkMode: true,
